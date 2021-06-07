b2b3bd647b50d92aef4fb0da6bb56638467954b1
Erik Hernandez <erikestuardo_161@hotmail.com>
2021-06-07 00:03:28 -0600

    add file GALILEO

---
+++ GALILEO/funcionesgalileo.js
@@ -0,0 +1,43 @@
+//*************************************************************** */
+//********F*****U*****N*****C*****I*****O*****N*****E*****S***** */
+//************************************************************* */
+
+
+
+
+
+
+
+//**=======================EJEMPLO1================================= */
+
+//funcion que despliegue en pantalla las vocales dos veces, una por una.
+function imprimirVocales() {
+    alert("a");
+    alert("b");
+    alert("c");
+    alert("d");
+    alert("e");
+}
+
+//se manda a llamar 2 veces la funcion para que se despliegue 2 veces las vocales 1 por una.
+imprimirVocales();
+imprimirVocales();
+
+//**================================================================ */
+
+
+
+
+
+
+
+//**=======================EJEMPLO2================================= */
+
+//Escribir un programa que muestre a un piloto las indicaciones para llegar desde el origen hasta el destino.//
+
+
+
+
+
+//**================================================================ */
+
