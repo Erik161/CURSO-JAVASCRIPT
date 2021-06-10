ce5e174cd02d9d18d119a87fd54ed4607086ee06
Erik Hernandez <erikestuardo_161@hotmail.com>
2021-06-09 23:32:13 -0600

    ejercicio 1

--- GALILEO/tarea1.js
+++ GALILEO/tarea1.js
@@ -1,3 +1,6 @@
+
+
+
 function histograma(numero1,numero2,numero3,numero4,numero5){
     var numero1 = "*****";  
     var numero2 = "***";
@@ -5,11 +8,7 @@ function histograma(numero1,numero2,numero3,numero4,numero5){
     var numero4 = "****";
     var numero5 = "*";
    
-    alert(numero1);
-    alert(numero2);
-    alert(numero3);
-    alert(numero4);
-    alert(numero5);
+    alert( numero1+ "\n"  +numero2+ "\n" +numero3+ "\n" +numero4+ "\n" +numero5);
   }
   
   histograma(1,2,6,7,4);
