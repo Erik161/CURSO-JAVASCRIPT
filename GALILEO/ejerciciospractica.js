d1440635b5c04c749a23111fd4e452c0cf5b4295
Erik Hernandez <erikestuardo_161@hotmail.com>
2021-06-22 03:04:14 -0600

    ejercicio1 ejerciciopractica

---
+++ GALILEO/ejerciciospractica.js
@@ -0,0 +1,21 @@
+
+//**==========================EJERCICIO 1 =============================== */
+
+function transmografo(num1,num2,num3){
+    let operacion = num1*num2;
+    let resultado = Math.pow(operacion,num3);
+    return console.log(resultado);
+  }
+  transmografo(5,3,6);
+//**======================================================================== */
+
+
+
+
+
+
+
+
+
+
+
