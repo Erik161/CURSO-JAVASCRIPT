4ca1170d835868fb31d55857ddd293986d9479e5
Erik Hernandez <erikestuardo_161@hotmail.com>
2021-06-21 22:12:07 -0600

    ejercicio 2 tarea 3

--- GALILEO/tarea3.js
+++ GALILEO/tarea3.js
@@ -3,3 +3,45 @@
 
 
 
+
+
+
+
+
+
+
+
+
+//**==========================EJERCICIO 2 =============================== */
+
+function generarnumeroaletorio(){
+    //*Se agrego el objeto Math.floor() para generar los numero enteros.
+    let numero= Math.floor(Math.random()*1000);
+    //Se agrego de un rango de 1 a 1000 para generar el numero aleatorio.
+}
+generarnumeroaletorio();
+
+//**======================================================================== */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
