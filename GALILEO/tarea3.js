ae0b7e8b58f026aad0e25a666d3839211a9aa8b8
Erik Hernandez <erikestuardo_161@hotmail.com>
2021-06-21 22:59:25 -0600

    ejericicio 2 tarea 3 return en console.log

--- GALILEO/tarea3.js
+++ GALILEO/tarea3.js
@@ -16,8 +16,9 @@
 
 function generarnumeroaletorio(){
     //*Se agrego el objeto Math.floor() para generar los numero enteros.
-    let numero= Math.floor(Math.random()*1000);
     //Se agrego de un rango de 1 a 1000 para generar el numero aleatorio.
+    let numero= Math.floor(Math.random()*1000);
+    return console.log(numero);
 }
 generarnumeroaletorio();
 
