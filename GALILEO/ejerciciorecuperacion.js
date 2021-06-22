f8ba27424b1a160c684d893ebee6e7ce8842e3ee
Erik Hernandez <erikestuardo_161@hotmail.com>
2021-06-22 03:15:29 -0600

    ejercici1 recuperacion

---
+++ GALILEO/ejerciciorecuperacion.js
@@ -0,0 +1,20 @@
+//**==========================EJERCICIO 1 =============================== */
+
+function palindromoidentificar(){
+    let cadena= prompt("Escriba una palabra: ").toLocaleLowerCase();
+    
+    cadena= cadena.replace(/ /g, "");
+
+    for (let i= 0; i<cadena.length;i++){
+        if(cadena[i]!=cadena[cadena.length-i-1]){
+            return alert("No es un Palindromo");
+        }
+    }
+    return alert("Si es un Palindromo");
+}
+
+palindromoidentificar();
+
+
+//**======================================================================== */
+
