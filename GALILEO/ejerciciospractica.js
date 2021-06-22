561a5ba6afe546b65ac938326d561c9844e54cd0
Erik Hernandez <erikestuardo_161@hotmail.com>
2021-06-22 09:18:51 -0600

    Update ejerciciospractica.js

--- GALILEO/ejerciciospractica.js
+++ GALILEO/ejerciciospractica.js
@@ -15,6 +15,33 @@ function transmografo(num1,num2,num3){
 
 
 
+//**==========================EJERCICIO 8 =============================== */
+
+function conteoVocales(frase){
+    let vocales = 'aeiouAEIOU';
+    let conteo =0;
+    for(let i=0; i<frase.length; i++){
+        if(vocales.indexOf(frase[i]) !== -1){
+            conteo +=1;
+        }
+    }
+    return console.log("Numero de vocales encontradas: "+conteo);
+   
+}
+
+conteoVocales('Hola Mundo');
+
+
+//**======================================================================== */
+
+
+
+
+
+
+
+
+
 
 
 
