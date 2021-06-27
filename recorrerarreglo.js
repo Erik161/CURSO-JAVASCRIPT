c4a66c3cf0a50b529b213d0c48889abe41de2efa
Erik Hernandez <erikestuardo_161@hotmail.com>
2021-06-26 23:58:47 -0600

    segunda forma

--- recorrerarreglo.js
+++ recorrerarreglo.js
@@ -8,3 +8,15 @@ colores = ["rojo","azul","negro"];
 for(i=0; i<colores.lenght; i++){
     console-log(colores[i]);
 }
+
+
+//SEGUDA FORMA
+
+colores = ["rojo","azul","negro"];
+
+colores.forEach(element => {
+    console.log(element)
+});
+
+
+
