e887e63a3bf93d46ab39b01af2b76b1b7eddc02f
Erik Hernandez <erikestuardo_161@hotmail.com>
2021-06-26 23:51:40 -0600

    reccorrer un arreglo 3 formas

---
+++ recorrerarreglo.js
@@ -0,0 +1,10 @@
+
+
+//===================== 3 FORMAS DE RECORRER UN ARRAY ==================
+
+//PRIMER FORMA
+colores = ["rojo","azul","negro"];
+
+for(i=0; i<colores.lenght; i++){
+    console-log(colores[i]);
+}
